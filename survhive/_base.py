3d74d086dcd67f73a9011a09a8a3a9d33cc924b4
slightlier <slightlier@gmail.com>
2022-10-14 18:06:29 +0200

    feat: Add base class

---
+++ survhive/_base.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model._base import LinearModel
+
+
+class RegularizedLinearSurvivalModel(LinearModel):
+    def fit(
+        self,
+        X: pd.DataFrame,
+        y: np.array,
+        sample_weight: np.array = None,
+        check_input: bool = True,
+    ) -> None:
+        """Fit model with proximal gradient descent.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of (n_samples, n_features)
+            Data.
+        y : np.array of shape (n_samples,)
+            Target. Will be cast to X's dtype if necessary.
+        sample_weight : np.array of shape (n_samples,), default=None
+            Sample weights. Internally, the `sample_weight` vector will be
+            rescaled to sum to `n_samples`.
+        check_input : bool, default=True
+            Allow to bypass several input checking.
+            Don't use this parameter unless you know what you do.
+
+        Notes
+        ---
+        To be implemented in each child class.
+        """
+        raise NotImplementedError
+
+    def predict_hazard_function(
+        self, X: pd.DataFrame, time: np.array
+    ) -> pd.DataFrame:
+        """Predict hazard function for each sample and each requested time.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of (n_samples, n_features)
+            Data.
+        time : np.array of (n_times)
+            Times at which hazard function predictions are desired.
+
+        Returns
+        ---
+        hazard_function : pd.DataFrame of (n_samples, n_times)
+            Hazard function for each sample and each requested time.
+
+        Notes
+        ---
+        To be implemented in each child class.
+        """
+        raise NotImplementedError
+
+    def predict_cumulative_hazard_function(
+        self, X: pd.DataFrame, time: np.array
+    ) -> pd.DataFrame:
+        """Predict cumulative hazard function for each sample and each requested time.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of (n_samples, n_features)
+            Data.
+        time : np.array of (n_times)
+            Times at which hazard function predictions are desired.
+
+        Returns
+        ---
+        cumulative_hazard_function : NDArray[Shape["*", "*"], Float32] of (n_samples, n_times)
+            Cumulative hazard function for each sample and each requested time.
+
+        Notes
+        ---
+        To be implemented in each child class.
+        """
+        raise NotImplementedError
+
+    def predict_survival_function(
+        self, X: pd.DataFrame, time: np.array
+    ) -> pd.DataFrame:
+        """Predict survival function for each sample and each requested time.
+
+        Parameters
+        ----------
+        X : pd.DataFrame of (n_samples, n_features)
+            Data.
+        time : np.array of (n_times)
+            Times at which hazard function predictions are desired.
+
+        Returns
+        ---
+        survival_function : pd.DataFrame of (n_samples, n_times)
+            Survival function for each sample and each requested time.
+
+        Notes
+        ---
+        To be implemented in each child class.
+        """
+        raise NotImplementedError
