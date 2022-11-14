58c2b9e75f0f3bba733e6cc1de857ef354d918ca
slightlier <slightlier@gmail.com>
2022-11-14 17:28:40 +0100

    feat: Implement API that allows to cache risk and death matrices when calculating the Cox PH gradient

--- survhive/cox.py
+++ survhive/cox.py
@@ -1,3 +1,4 @@
+from itertools import partial
 from typing import Callable, List, Optional, Union
 
 import numpy as np
@@ -5,17 +6,15 @@ import pandas as pd
 
 from ._base import RegularizedLinearSurvivalModel
 from .gradients import GRADIENT_FACTORY
-from .proximal_operators import ProximalOperator
-from .utils import get_closest
+from .utils import get_closest, get_death_matrix, get_risk_matrix
 
 
 class CoxPH(RegularizedLinearSurvivalModel):
     def __init__(
         self,
-        alpha: Optional[float],
         threshold: float,
         groups: List[Union[int, List[int]]],
-        proximal_operator: ProximalOperator,
+        proximal_operator: str = "lasso",
         scale_group: Optional[str] = "group_length",
         solver: str = "copt",
         warm_start: bool = True,
@@ -23,11 +22,14 @@ class CoxPH(RegularizedLinearSurvivalModel):
         tol: float = 1e-7,
         verbose: int = 0,
         random_state: Optional[int] = None,
+        psi: float = 0.0,
+        alpha: Optional[float] = None,
+        gamma: Optional[float] = 4.0,
+        tau: Optional[float] = 1 / 3,
         tie_correction: str = "efron",
         baseline_hazard_estimator: str = "breslow",
     ) -> None:
         super().__init__(
-            alpha=alpha,
             threshold=threshold,
             groups=groups,
             proximal_operator=proximal_operator,
@@ -38,30 +40,62 @@ class CoxPH(RegularizedLinearSurvivalModel):
             tol=tol,
             verbose=verbose,
             random_state=random_state,
-            tie_correction=tie_correction,
-            baseline_hazard_estimator=baseline_hazard_estimator,
+            psi=psi,
+            alpha=alpha,
+            gamma=gamma,
+            tau=tau,
         )
-        if not isinstance(tie_correction, str):
-            raise ValueError(
-                f"`tie_correction` must be of type `str`. Found type {type(tie_correction)} instead."
-            )
-        if tie_correction not in ["efron", "breslow"]:
-            raise ValueError(
-                f"`tie_correction` must be one of ['efron', 'breslow']. Found {tie_correction} instead."
-            )
+
         self.tie_correction: str = tie_correction
-        self.grad: Callable[
-            [np.array, pd.DataFrame, np.array], np.array
-        ] = GRADIENT_FACTORY[tie_correction]
-        if not isinstance(baseline_hazard_estimator, str):
-            raise ValueError(
-                f"`baseline_hazard_estimator` must be of type `str`. Found type {type(baseline_hazard_estimator)} instead."
-            )
-        if baseline_hazard_estimator not in ["breslow"]:
-            raise ValueError(
-                f"`baseline_hazard_estimator` must be one of ['breslow']. Found {baseline_hazard_estimator} instead."
-            )
         self.baseline_hazard_estimator: str = baseline_hazard_estimator
+
+    def fit(
+        self,
+        X: pd.DataFrame,
+        y: np.array,
+        sample_weight: np.array = None,
+        check_input: bool = True,
+    ) -> None:
+        if not isinstance(self.tie_correction, str):
+            raise ValueError(
+                f"`tie_correction` must be of type `str`. Found type {type(self.tie_correction)} instead."
+            )
+        if self.tie_correction not in ["efron", "breslow"]:
+            raise ValueError(
+                f"`tie_correction` must be one of ['efron', 'breslow']. Found {self.tie_correction} instead."
+            )
+        if not isinstance(self.baseline_hazard_estimator, str):
+            raise ValueError(
+                f"`baseline_hazard_estimator` must be of type `str`. Found type {type(self.baseline_hazard_estimator)} instead."
+            )
+        if self.baseline_hazard_estimator not in ["breslow"]:
+            raise ValueError(
+                f"`baseline_hazard_estimator` must be one of ['breslow']. Found {self.baseline_hazard_estimator} instead."
+            )
+
+        # Freeze all arguments of the gradient that we are optimising
+        # except for beta. Specifically, cache objects such as
+        # the risk and death matrix which are semi-expensive to compute.
+        if self.tie_correction == "efron":
+            risk_matrix: np.array = get_risk_matrix()
+            death_matrix: np.array = get_death_matrix()
+            self.grad: Callable[[np.array], np.array] = partial(
+                GRADIENT_FACTORY[self.tie_correction],
+                X=X,
+                risk_matrix=risk_matrix,
+                death_matrix=death_matrix,
+            )
+        elif self.tie_correction == "breslow":
+            risk_matrix: np.array = get_risk_matrix()
+            self.grad: Callable[[np.array], np.array] = partial(
+                GRADIENT_FACTORY[self.tie_correction],
+                X=X,
+                y=y,
+                risk_matrix=risk_matrix,
+            )
+        return super().fit(
+            X=X, y=y, sample_weight=sample_weight, check_input=check_input
+        )
 
     def predict_cumulative_hazard_function(
         self, X: pd.DataFrame, time: np.array
