6c65d572e24db260c616ef0af5ac3c0eb2fa85df
slightlier <slightlier@gmail.com>
2023-02-14 09:49:49 +0100

    refactor: Refactor base class to match new optimiser

--- survhive/_base.py
+++ survhive/_base.py
@@ -1,64 +1,42 @@
 from typing import List, Optional, Union
 
-import copt as cp
 import numpy as np
 import pandas as pd
 from sklearn.linear_model._base import LinearModel
+from typeguard import typechecked
 
-from .baseline_hazard import CUMULATIVE_BASELINE_HAZARD_FACTORY
-from .proximal_operators import ElasticNetProximal
-from .utils import (
-    _check_groups,
-    _check_max_iter,
-    _check_nu,
-    _check_penalty_factors,
-    _check_proximal_operator,
-    _check_solver,
-    _check_tol,
-    _check_verbose,
-    summarise_groups,
-)
+from .optimiser import Optimiser
 
 
+@typechecked
 class RegularizedLinearSurvivalModel(LinearModel):
     def __init__(
         self,
-        proximal_operator: str,
-        groups: List[List[int]],
-        threshold: float,
-        scale_group: Optional[str] = "group_length",
-        penalty_factors: Optional[np.array] = None,
-        solver: str = "copt",
+        alpha: float,
+        optimiser: str,
+        l1_ratio: float = 1.0,
+        groups: Optional[List[List[int]]] = None,
+        line_search: bool = True,
+        line_search_reduction_factor: float = 0.5,
+        warm_start: bool = True,
         max_iter: int = 1000,
         tol: float = 1e-7,
         verbose: int = 0,
-        psi: float = 0.0,
-        alpha: Optional[float] = None,
-        gamma: Optional[float] = 4.0,
-        tau: Optional[float] = 1 / 3,
-        nu: Optional[float] = 1.0,
+        random_state: Optional[Union[int, np.random.RandomState]] = None,
     ):
-        self.proximal_operator = proximal_operator
-        self.groups = groups
-        self.threshold = threshold
-        self.scale_group = scale_group
-        self.penalty_factors = penalty_factors
-        self.solver = solver
-        self.max_iter = max_iter
-        self.tol = tol
-        self.verbose = verbose
-        self.psi = psi
-        self.alpha = alpha
-        self.gamma = gamma
-        self.tau = tau
-        self.nu = nu
+        self.alpha: float = alpha
+        self.optimiser: str = optimiser
+        self.l1_ratio: float = l1_ratio
+        self.groups: Optional[List[List[int]]] = groups
+        self.line_search: bool = line_search
+        self.line_search_reduction_factor: float = line_search_reduction_factor
+        self.warm_start: bool = warm_start
+        self.max_iter: int = max_iter
+        self.tol: float = tol
+        self.verbose: int = verbose
+        self.random_state: Optional[Union[int, np.random.RandomState]] = random_state
 
-        # By default, we have not calculated the baseline
-        # cumulative hazard yet, so we set the caching tracker
-        # to false.
-        self.baseline_cumulative_hazard_cached = False
-
-    def fit(self, X: pd.DataFrame, y: np.array) -> None:
+    def fit(self, X: pd.DataFrame, y: np.array, sample_weight: np.array = None) -> None:
         """Fit model with proximal gradient descent.
 
         Parameters
@@ -78,65 +56,19 @@ class RegularizedLinearSurvivalModel(LinearModel):
         ---
         To be implemented in each child class.
         """
-        penalty_factors = _check_penalty_factors(self.penalty_factors)
-        proximal_operator = _check_proximal_operator(
-            proximal_operator=self.proximal_operator,
-            groups=self.groups,
-            scale_group=self.scale_group,
-            penalty_factors=penalty_factors,
-            threshold=self.threshold,
+        optimiser: Optimiser = Optimiser(
+            grad=self.gradient,
+            optimiser=self.optimiser,
+            line_search=self.line_search,
+            max_iter=self.max_iter,
+            tol=self.tol,
+            random_state=self.random_state,
             alpha=self.alpha,
-            gamma=self.gamma,
-            tau=self.tau,
+            groups=self.groups,
         )
-        nu = _check_nu(nu=self.nu)
-        if nu:
-            proximal_operator = ElasticNetProximal(
-                proximal_operator=proximal_operator,
-                threshold=self.threshold * penalty_factors,
-                nu=nu,
-            )
+        self.coef_: np.array = optimiser.optimise(X=X, y=y, sample_weight=sample_weight)
 
-        solver = _check_solver(self.solver)
-        max_iter = _check_max_iter(self.max_iter)
-        tol = _check_tol(self.max_tol)
-        verbose = _check_verbose(self.verbose)
-        self.n_features_ = X.shape[1]
-        coef_ = np.zeros(len(self.groups))
-        if solver == "copt":
-            pgd = cp.minimize_proximal_gradient(
-                self.grad,
-                coef_,
-                proximal_operator,
-                jac=True,
-                step="backtracking",
-                max_iter=max_iter,
-                tol=tol,
-                verbose=verbose,
-                callback=None,
-                accelerated=False,
-            )
-        elif solver == "numba":
-            raise NotImplementedError
-
-        if self.has_overlaps:
-            pgd.x = summarise_groups(
-                coef=pgd.x,
-                inverse_groups=self.inverse_groups,
-            )
-        self.coef_: np.array = pgd.x
-        self.is_fitted: bool = True
-        self.baseline_cumulative_hazard: np.array
-        self.baseline_cumulative_hazard_times: np.array
-        (
-            self.baseline_cumulative_hazard,
-            self.baseline_cumulative_hazard_times,
-        ) = CUMULATIVE_BASELINE_HAZARD_FACTORY[self.baseline_hazard_estimator](
-            X=X, y=y, coef=self.coef_
-        )
-        self.baseline_cumulative_hazard_cached = True
-
-    def predict_hazard_function(self, X: pd.DataFrame, time: np.array) -> pd.DataFrame:
+    def predict_hazard_function(self, X: np.array, time: np.array) -> pd.DataFrame:
         """Predict hazard function for each sample and each requested time.
 
         Parameters
@@ -158,7 +90,7 @@ class RegularizedLinearSurvivalModel(LinearModel):
         raise NotImplementedError
 
     def predict_cumulative_hazard_function(
-        self, X: pd.DataFrame, time: np.array
+        self, X: np.array, time: np.array
     ) -> pd.DataFrame:
         """Predict cumulative hazard function for each sample and each requested time.
 
@@ -180,9 +112,7 @@ class RegularizedLinearSurvivalModel(LinearModel):
         """
         raise NotImplementedError
 
-    def predict_survival_function(
-        self, X: pd.DataFrame, time: np.array
-    ) -> pd.DataFrame:
+    def predict_survival_function(self, X: np.array, time: np.array) -> pd.DataFrame:
         """Predict survival function for each sample and each requested time.
 
         Parameters
