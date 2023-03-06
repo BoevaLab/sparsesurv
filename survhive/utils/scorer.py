f309b858d859361e565e27e1eceb773d3191b07f
Nikita Janakarajan <nja@zurich.ibm.com>
2023-03-06 23:24:49 +0100

    chore: typing, docstrings, decorators

--- survhive/utils/scorer.py
+++ survhive/utils/scorer.py
@@ -1,18 +1,24 @@
 import numpy as np
+from typeguard import typechecked
+
 from survhive.utils import *
 
 
-def get_partial_log_likelihood(z, time_event) -> float:
+@typechecked
+@jit(nopython=True, cache=True)
+def get_partial_log_likelihood(z: np.ndarray, time_event: np.ndarray) -> float:
     """Computes the partial log likelihood of given data.
 
     Args:
-        z (_type_): _description_
-        time_event (_type_): _description_
+        z (np.ndarray): 2-dimensional array of linear predictors of size (samples, num_coefs).
+        time_event (np.ndarray): 2-dimensional array of time and event survival data.
+            The array is of shape (samples, target), where targets are in str format.
 
     Returns:
-        float: _description_
+        float: Scalar value summarising the total partial log-likelihood of the given
+            linear predictors and their corresponding event and time data.
     """
-    # this assumes no ties. Q - should divide by n?
+    # INFO: this assumes no ties. Q - should we divide by n?
     time, event = inverse_transform_survival_target(time_event)
     sorted_indices = np.argsort(a=time, kind="stable")
     time_sorted = time[sorted_indices]
@@ -31,36 +37,47 @@ def get_partial_log_likelihood(z, time_event) -> float:
     return log_likelihood
 
 
-def basic_cv(z_test, y_test, **kwargs) -> float:
+@jit(nopython=True, cache=True)
+def basic_cv(z_test: np.ndarray, y_test: np.ndarray, **kwargs) -> float:
     """Basic CV scoring function.
 
     Args:
-        z_test (_type_): _description_
-        y_test (_type_): _description_
+        z_test (np.ndarray): 3-dimensional array of the linear predictors of all test folds.
+            The array is of shape (folds, samples, predictors).
+        y_test (np.ndarray): 3-dimensional array of survival time and event data
+            corresponding to samples in all test folds. The array is of shape
+            (folds, samples, target).
 
     Returns:
-        float: _description_
+        float: Scalar value of the mean partial log-likelihoods across all test folds.
     """
     folds, _, _ = z_test.shape
     test_fold_likelihoods = []
     for k in range(folds):
         test_fold_likelihoods.append(
-            get_partial_log_likelihood(z_test[k, :, :], y_test[k, :])
+            get_partial_log_likelihood(z_test[k, :, :], y_test[k, :, :])
         )
 
     return np.mean(test_fold_likelihoods)
-    # return test_fold_likelihoods
 
 
-def vvh_cv(z_test, y_test, **kwargs) -> float:
+@jit(nopython=True, cache=True)
+def vvh_cv(z_test: np.ndarray, y_test: np.ndarray, **kwargs) -> float:
     """Verweij and Van Houwelingen CV scoring function.
 
     Args:
-        z_test (_type_): _description_
-        y_test (_type_): _description_
+        z_test (np.ndarray): 3-dimensional array of the linear predictors of all test folds.
+            The array is of shape (folds, samples, predictors).
+        y_test (np.ndarray): 3-dimensional array of survival time and event data
+            corresponding to samples in all test folds. The array is of shape
+            (folds, samples, target).
+        kwargs : VVH method requires that the linear predictors and targets of the
+            training folds are also passed as addiitonal keyword arguments. For linear
+            predictors of the training data, use "z_train" as the keyword, and for the
+            correspondig targets, use "y_train" as the keyword when calling the function.
 
     Returns:
-        float: _description_
+        float: Scalar value of the mean partial log-likelihoods across all test folds.
     """
     z_train = kwargs["z_train"]
     y_train = kwargs["y_train"]
@@ -69,25 +86,30 @@ def vvh_cv(z_test, y_test, **kwargs) -> float:
 
     for k in range(folds):
         z = np.append(z_train[k, :, :], z_test[k, :, :])
-        y = np.append(y_train[k, :], y_test[k, :])
+        y = np.append(y_train[k, :, :], y_test[k, :, :])
 
         z_likelihood = get_partial_log_likelihood(z, y)
-        z_train_likelihood = get_partial_log_likelihood(z_train[k, :, :], y_train[k, :])
+        z_train_likelihood = get_partial_log_likelihood(
+            z_train[k, :, :], y_train[k, :, :]
+        )
         test_fold_likelihoods.append(z_likelihood - z_train_likelihood)
 
-    # return test_fold_likelihoods
     return np.mean(test_fold_likelihoods)
 
 
-def linear_cv(z_test, y_test, **kwargs) -> float:
-    """_summary_
+@jit(nopython=True, cache=True)
+def linear_cv(z_test: np.ndarray, y_test: np.ndarray, **kwargs) -> float:
+    """CV score computation using linear predictors (Dai et. al. 2019).
 
     Args:
-        z_test (_type_): _description_
-        y_test (_type_): _description_
+        z_test (np.ndarray): 3-dimensional array of the linear predictors of all test folds.
+            The array is of shape (folds, samples, predictors).
+        y_test (np.ndarray): 3-dimensional array of survival time and event data
+            corresponding to samples in all test folds. The array is of shape
+            (folds, samples, target).
 
     Returns:
-        float: _description_
+        float: Scalar value of the partial log-likelihoods across all test folds.
     """
 
     assert z_test.ndim == 3
