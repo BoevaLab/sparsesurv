[FIX] Misc and stable Cox gradients

survhive/_utils_/cv.py
@@ -44,8 +44,8 @@ def _alpha_grid(
     gradient,
     hessian,
     Xy: ArrayLike = None,
-    l1_ratio: float = 0.9,
-    eps: float = 0.1,
+    l1_ratio: float = 1.0,
+    eps: float = 0.01,
     n_alphas: int = 100,
 ) -> np.array:
     """Compute the grid of alpha values for model parameter search
@@ -74,7 +74,6 @@ def _alpha_grid(
 
     if Xy.ndim == 1:
         Xy = Xy[:, np.newaxis]
-    hessian_mask: np.array = (hessian > 0).astype(bool)
     alpha_max = np.max(np.abs(np.matmul(gradient.T, X))) / l1_ratio
     if alpha_max <= np.finfo(float).resolution:
         alphas = np.empty(n_alphas)
@@ -83,8 +82,8 @@ def _alpha_grid(
 
     alphas = np.round(
         np.logspace(
-            np.log10((alpha_max + EPS) * eps),
-            np.log10(alpha_max + EPS),
+            np.log10((alpha_max) * eps),
+            np.log10(alpha_max),
             num=n_alphas,
         )[::-1],
         decimals=10,
@@ -167,7 +166,7 @@ def regularisation_path(
     coefs = np.zeros((n_features, n_alphas), dtype=X.dtype)
     train_eta = np.empty((n_samples, n_alphas), dtype=X.dtype)
     test_eta = np.empty((test_samples, n_alphas), dtype=X.dtype)
-
+    print(alphas)
     eta_previous_alpha = np.zeros(X.shape[0])
     active_variables = np.empty(0, dtype=int)
     for i, alpha in enumerate(alphas):
@@ -215,6 +214,7 @@ def regularisation_path(
                     weights_scaled,
                     strong_screener.working_set,
                 )
+                # strong_screener.expand_working_set_raw(strong_screener.strong_set)
             # For the first alpha, we calculate our optimiser
             # without any screening.
             if i == 0:
@@ -662,7 +662,7 @@ class CrossValidation(LinearModelCV):
                     n_alphas=self.n_alphas,
                     gradient=gradient,
                     hessian=hessian,
-                    # l1_ratio=l1_ratio,
+                    l1_ratio=l1_ratio,
                 )
                 for l1_ratio in l1_ratios
             ]
@@ -731,11 +731,12 @@ class CrossValidation(LinearModelCV):
                         model.loss,
                     )
                     print(f"Alpha: {j}/100")
+                    print(likelihood)
                     if np.isnan(likelihood):
-                        print(j)
-                        print(alphas[i][j])
-                        print(test_eta_method[:, j])
-                        print("HEYOOOO")
+                        # print(j)
+                        # print(alphas[i][j])
+                        # print(test_eta_method[:, j])
+                        # print("HEYOOOO")
                         # raise ValueError
                         mean_cv_score_l1.append(-np.inf)
                     else:

survhive/baseline_hazard_estimation.py
@@ -1,4 +1,5 @@
 from math import log
+import math
 
 import numpy as np
 from numba import jit
@@ -76,7 +77,6 @@ def breslow_estimator_efron(
 
         if sample_time > previous_time and local_death_set:
             for ell in range(local_death_set):
-
                 cumulative_baseline_hazards[n_events_counted] += 1 / (
                     local_risk_set - (ell / local_death_set) * local_death_set_risk
                 )
@@ -110,31 +110,27 @@ def baseline_hazard_estimator_aft(
     train_time,
     train_event,
     train_eta,
-    bandwidth_function: str = "jones_1990",
 ):
-    if bandwidth_function == "jones_1990":
-        bandwidth: float = jones_1990(time=train_time, event=train_event)
-    else:
-        bandwidth: float = jones_1991(time=train_time, event=train_event)
-
     n_samples: int = train_time.shape[0]
+    bandwidth = 1.30 * math.pow(n_samples, -0.2)
     inverse_bandwidth: float = 1 / bandwidth
     inverse_sample_size: float = 1 / n_samples
-    inverse_bandwidth_sample_size: float = inverse_bandwidth * inverse_sample_size
     log_time: float = log(time + EPS)
+    inverse_bandwidth_sample_size_time: float = (
+        inverse_sample_size * (1 / (time + EPS)) * inverse_bandwidth
+    )
+
     R_lp: np.array = np.log(train_time * np.exp(train_eta))
     difference_lp_log_time: np.array = (R_lp - log_time) / bandwidth
     numerator: float = 0.0
     denominator: float = 0.0
-
     for _ in range(n_samples):
-        difference: float = difference_lp_log_time[_]
-        denominator += gaussian_integrated_kernel(difference)
+        difference_div: float = difference_lp_log_time[_]
+        denominator += gaussian_integrated_kernel(difference_div)
         if train_event[_]:
-            numerator += gaussian_kernel(difference)
-    numerator = inverse_bandwidth_sample_size * numerator
+            numerator += gaussian_kernel(difference_div)
+    numerator = inverse_bandwidth_sample_size_time * numerator
     denominator = inverse_sample_size * denominator
-
     return numerator / denominator
 
 
@@ -144,26 +140,22 @@ def baseline_hazard_estimator_ah(
     train_time,
     train_event,
     train_eta,
-    bandwidth_function: str = "jones_1990",
 ):
-    if bandwidth_function == "jones_1990":
-        bandwidth: float = jones_1990(time=train_time, event=train_event)
-    else:
-        bandwidth: float = jones_1991(time=train_time, event=train_event)
     n_samples: int = train_time.shape[0]
+    bandwidth = 1.30 * math.pow(n_samples, -0.2)
     inverse_bandwidth: float = 1 / bandwidth
     inverse_sample_size: float = 1 / n_samples
-    inverse_bandwidth_sample_size: float = inverse_bandwidth * inverse_sample_size
-    log_time: float = log(time + EPS)
-    exp_linear_predictor: np.array = np.exp(train_eta)
-    R_lp: np.array = np.log(train_time * exp_linear_predictor)
-    difference_lp_log_time: np.array = R_lp - log_time
+    inverse_bandwidth_sample_size: float = (
+        inverse_sample_size * (1 / (time + EPS)) * inverse_bandwidth
+    )
+    log_time: float = time
+    R_lp: np.array = np.log(train_time * np.exp(train_eta))
+    difference_lp_log_time: np.array = (R_lp - log_time) / bandwidth
     numerator: float = 0.0
     denominator: float = 0.0
-
     for _ in range(n_samples):
         difference: float = difference_lp_log_time[_]
-        denominator += exp_linear_predictor[_] * gaussian_integrated_kernel(difference)
+        denominator += np.exp(-train_eta) * gaussian_integrated_kernel(difference)
         if train_event[_]:
             numerator += gaussian_kernel(difference)
     numerator = inverse_bandwidth_sample_size * numerator

survhive/gradients.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import numpy as np
 from numba import jit
 
-from .constants import CDF_ZERO, PDF_PREFACTOR
+from .constants import CDF_ZERO, PDF_PREFACTOR, EPS
 from .utils import difference_kernels
 import math
 
@@ -604,6 +604,95 @@ def breslow_numba(
 
 
 @jit(nopython=True, cache=True, fastmath=True)
+def breslow_numba_stable(
+    linear_predictor: np.array,
+    time: np.array,
+    event: np.array,
+) -> Tuple[np.array, np.array]:
+    """Gradient of the Breslow approximated version of CoxPH in numba-compatible form.
+
+    Args:
+        linear_predictor (np.array): Linear predictor of risk: `X @ coef`. Shape = (n_samples,).
+        time (np.array): Array containing event/censoring times of shape = (n_samples,).
+        event (np.array): Array containing binary event indicators of shape = (n_samples,).
+
+    Returns:
+        Tuple[np.array, np.array]: Tuple containing the negative gradients and the hessian
+            of with the linear predictor.
+    """
+    # Assumes times have been sorted beforehand.
+    partial_hazard = np.exp(linear_predictor - np.max(linear_predictor))
+    partial_hazard[partial_hazard < EPS] = EPS
+    samples = time.shape[0]
+    risk_set_sum = 0
+    for i in range(samples):
+        risk_set_sum += partial_hazard[i]
+
+    grad = np.empty(samples)
+    hess = np.empty(samples)
+    previous_time = time[0]
+    local_risk_set = 0
+    local_risk_set_hessian = 0
+    death_set_count = 0
+    censoring_set_count = 0
+    accumulated_sum = 0
+
+    for i in range(samples):
+        sample_time = time[i]
+        sample_event = event[i]
+        sample_partial_hazard = partial_hazard[i]
+        # print(f"Sample: {i+1} / X")
+        # print(local_risk_set)
+        if previous_time < sample_time:
+            if death_set_count:
+                (local_risk_set, local_risk_set_hessian,) = update_risk_sets_breslow(
+                    risk_set_sum,
+                    death_set_count,
+                    local_risk_set,
+                    local_risk_set_hessian,
+                )
+            for death in range(death_set_count + censoring_set_count):
+                death_ix = i - 1 - death
+                (grad[death_ix], hess[death_ix],) = calculate_sample_grad_hess(
+                    partial_hazard[death_ix],
+                    event[death_ix],
+                    local_risk_set,
+                    local_risk_set_hessian,
+                )
+
+            risk_set_sum -= accumulated_sum
+            accumulated_sum = 0
+            death_set_count = 0
+            censoring_set_count = 0
+
+        if sample_event:
+            death_set_count += 1
+        else:
+            censoring_set_count += 1
+
+        accumulated_sum += sample_partial_hazard
+        previous_time = sample_time
+
+    i += 1
+    if death_set_count:
+        local_risk_set, local_risk_set_hessian = update_risk_sets_breslow(
+            risk_set_sum,
+            death_set_count,
+            local_risk_set,
+            local_risk_set_hessian,
+        )
+    for death in range(death_set_count + censoring_set_count):
+        death_ix = i - 1 - death
+        (grad[death_ix], hess[death_ix],) = calculate_sample_grad_hess(
+            partial_hazard[death_ix],
+            event[death_ix],
+            local_risk_set,
+            local_risk_set_hessian,
+        )
+    return grad / samples, hess / samples
+
+
+@jit(nopython=True, cache=True, fastmath=True)
 def calculate_sample_grad_hess_efron(
     sample_partial_hazard: float,
     sample_event: int,
@@ -635,6 +724,31 @@ def calculate_sample_grad_hess_efron(
         return ((sample_partial_hazard) * local_risk_set), (
             sample_partial_hazard
         ) * local_risk_set - local_risk_set_hessian * ((sample_partial_hazard) ** 2)
+
+
+@jit(nopython=True, cache=True, fastmath=True)
+def calculate_sample_grad_hess(
+    sample_partial_hazard: float,
+    sample_event: int,
+    local_risk_set: float,
+    local_risk_set_hessian: float,
+) -> Tuple[float, float]:
+    """_summary_
+
+    Args:
+        sample_partial_hazard (float): _description_
+        sample_event (int): _description_
+        local_risk_set (float): _description_
+        local_risk_set_hessian (float): _description_
+
+    Returns:
+        Tuple[float, float]: _description_
+    """
+    return (
+        sample_partial_hazard * local_risk_set
+    ) - sample_event, sample_partial_hazard * local_risk_set - local_risk_set_hessian * (
+        sample_partial_hazard**2
+    )
 
 
 @jit(nopython=True, cache=True, fastmath=True)
@@ -789,4 +903,120 @@ def efron_numba(
             local_risk_set_death,
             local_risk_set_hessian_death,
         )
+    return grad / samples, hess / samples
+
+
+@jit(nopython=True, cache=True, fastmath=True)
+def efron_numba_stable(
+    linear_predictor: np.array,
+    time: np.array,
+    event: np.array,
+) -> Tuple[np.array, np.array]:
+    """Gradient of the Efron approximated version of CoxPH in numba-compatible form.
+
+    Args:
+        linear_predictor (np.array): Linear predictor of risk: `X @ coef`. Shape = (n_samples,).
+        time (np.array): Array containing event/censoring times of shape = (n_samples,).
+        event (np.array): Array containing binary event indicators of shape = (n_samples,).
+
+    Returns:
+        Tuple[np.array, np.array]: Tuple containing the negative gradients and the hessian
+            of with the linear predictor.
+    """
+    # Assumes times have been sorted beforehand.
+    partial_hazard = np.exp(linear_predictor - np.max(linear_predictor))
+    partial_hazard[partial_hazard < EPS] = EPS
+    samples = time.shape[0]
+    risk_set_sum = 0
+    grad = np.empty(samples)
+    hess = np.empty(samples)
+    previous_time: float = time[0]
+    local_risk_set: int = 0
+    local_risk_set_hessian: int = 0
+    death_set_count: int = 0
+    censoring_set_count: int = 0
+    accumulated_sum: int = 0
+    death_set_risk: float = 0.0
+    local_risk_set_death: float = 0.0
+    local_risk_set_hessian_death: float = 0.0
+
+    for i in range(samples):
+        risk_set_sum += partial_hazard[i]
+
+    # print(risk_set_sum)
+    for i in range(samples):
+        sample_time: float = time[i]
+        sample_event: int = event[i]
+        sample_partial_hazard: float = partial_hazard[i]
+
+        if previous_time < sample_time:
+            if death_set_count:
+                (
+                    local_risk_set,
+                    local_risk_set_hessian,
+                    local_risk_set_death,
+                    local_risk_set_hessian_death,
+                ) = update_risk_sets_efron_pre(
+                    risk_set_sum,
+                    death_set_count,
+                    local_risk_set,
+                    local_risk_set_hessian,
+                    death_set_risk,
+                )
+            for death in range(death_set_count + censoring_set_count):
+                death_ix = i - 1 - death
+                (grad[death_ix], hess[death_ix],) = calculate_sample_grad_hess_efron(
+                    partial_hazard[death_ix],
+                    event[death_ix],
+                    local_risk_set,
+                    local_risk_set_hessian,
+                    local_risk_set_death,
+                    local_risk_set_hessian_death,
+                )
+            risk_set_sum -= accumulated_sum
+            accumulated_sum = 0
+            death_set_count = 0
+            censoring_set_count = 0
+            death_set_risk = 0
+            local_risk_set_death = 0
+            local_risk_set_hessian_death = 0
+
+        if sample_event:
+            death_set_count += 1
+            death_set_risk += sample_partial_hazard
+        else:
+            censoring_set_count += 1
+
+        accumulated_sum += sample_partial_hazard
+        previous_time = sample_time
+
+    i += 1
+    if death_set_count:
+        (
+            local_risk_set,
+            local_risk_set_hessian,
+            local_risk_set_death,
+            local_risk_set_hessian_death,
+        ) = update_risk_sets_efron_pre(
+            risk_set_sum,
+            death_set_count,
+            local_risk_set,
+            local_risk_set_hessian,
+            death_set_risk,
+        )
+    for death in range(death_set_count + censoring_set_count):
+        death_ix = i - 1 - death
+        (grad[death_ix], hess[death_ix],) = calculate_sample_grad_hess_efron(
+            partial_hazard[death_ix],
+            event[death_ix],
+            local_risk_set,
+            local_risk_set_hessian,
+            local_risk_set_death,
+            local_risk_set_hessian_death,
+        )
+    if np.min(hess) < 0:
+        # print(parit)
+        print(partial_hazard)
+        print(np.exp(linear_predictor - np.max(linear_predictor)))
+        raise ValueError
     return grad / samples, hess / samples