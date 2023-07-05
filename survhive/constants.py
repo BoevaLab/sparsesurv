3cd16abd60adc7596c59f01f52bb4e290ee9a42f
slightlier <slightlier@gmail.com>
2023-07-05 14:46:53 +0200

    feat: Switch focus of package solely to preconditioning

--- survhive/constants.py
+++ survhive/constants.py
@@ -1,3 +1,7 @@
 from typing import Final
 
+PDF_PREFACTOR: Final[float] = 0.3989424488876037
+SQRT_TWO: Final[float] = 1.4142135623730951
+SQRT_EPS: Final[float] = 1.4901161193847656e-08
 EPS: Final[float] = 2.220446049250313e-16
+CDF_ZERO: Final[float] = 0.5
