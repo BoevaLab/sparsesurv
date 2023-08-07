5920920e4561f156d691459e9758685faa2ab0ff
Nikita Janakarajan <nja@zurich.ibm.com>
2023-08-07 18:54:24 +0200

    merge conflict resolved

--- survhive/tests/test_efron.py
+++ survhive/tests/test_efron.py
@@ -4,8 +4,8 @@ import sys
 import numpy as np
 import pandas as pd
 import pytest
-from scipy.optimize import check_grad
-from test_data_gen_final import numpy_test_data_1d, numpy_test_data_2d
+
+from test_data_gen_final import numpy_test_data_1d
 
 from survhive.loss import efron_negative_likelihood
 from math import log
@@ -49,65 +49,71 @@ def efron_calculation(linear_predictor, time, event):
 
 
 class TestEfronLoss:
-
     def test_default(self):
         linear_predictor, time, event = numpy_test_data_1d("default")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for default data."
 
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for default data."
 
     def test_first_five_zero(self):
-        
         linear_predictor, time, event = numpy_test_data_1d("first_five_zero")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: first five zero events."
+
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: first five zero events."
 
     def test_last_five_zero(self):
-
         linear_predictor, time, event = numpy_test_data_1d("last_five_zero")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: last five zero events."
+
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: last five zero events."
 
     def test_high_event_ratio(self):
-
         linear_predictor, time, event = numpy_test_data_1d("high_event_ratio")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: high event ratio."
+
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: high event ratio."
 
     def test_low_event_ratio(self):
-
         linear_predictor, time, event = numpy_test_data_1d("low_event_ratio")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: low event ratio."
+
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: low event ratio."
 
     def test_all_events(self):
-
         linear_predictor, time, event = numpy_test_data_1d("all_events")
-        
+
         efron_formula_computation = efron_calculation(linear_predictor, time, event)
         efron_loss = efron_negative_likelihood(linear_predictor, time, event)
-        
-        assert np.allclose(efron_loss,efron_formula_computation, atol=1e-2), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: all(100%) events."
+
+        assert np.allclose(
+            efron_loss, efron_formula_computation, atol=1e-2
+        ), f"Computed efron loss is {efron_loss} but formula yields {efron_formula_computation} for edge case: all(100%) events."
 
     def test_no_events(self):
-
         linear_predictor, time, event = numpy_test_data_1d("no_events")
-        
+
         with pytest.raises(RuntimeError) as excinfo:
             efron_negative_likelihood(linear_predictor, time, event)
-        assert "No events detected!" in str(excinfo.value), f"Events detected in data. Check data or the function <efron_negative_likelihood> to make sure data is processed correctly."
+        assert "No events detected!" in str(
+            excinfo.value
+        ), "Events detected in data. Check data or the function <efron_negative_likelihood> to make sure data is processed correctly."
