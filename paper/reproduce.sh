da63db95121edeebe1d81286b72b2d7295a2b23a
Nikita Janakarajan <nja@zurich.ibm.com>
2023-10-26 12:32:27 +0200

    chore: removed paper

--- paper/reproduce.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bash scripts/sh/create_paths.sh &> create_paths.log
-bash scripts/sh/download_data.sh &> download_data.log
-bash scripts/sh/preprocess_data.sh &> preprocess_data.log
-bash scripts/sh/make_splits.sh &> make_splits.log
-bash scripts/sh/rerun_experiments.sh &> reproduce_experiments.log
--- paper/scripts/sh/make_splits.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./data/splits
-
-python -u scripts/py/rerun_splits.py \
-    --data_dir ./data/ \
-    --config_path ./
